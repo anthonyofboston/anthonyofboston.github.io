cc8a89fa53ab05b991d031d55068eeeed3ec1543
anthonyofboston <110181255+anthonyofboston@users.noreply.github.com>
2024-12-11 18:42:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ function startmic() {
   introSection.style.display = "none";
   setupModel(URL, data => {
     let maximum = Math.max(...data);
-    if (maximum > 0.9) {
+    if (maximum > 0.8) {
        
       switch (maximum) {
               
