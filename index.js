06d4d873e2849587e0c5106c0a6e8ecf9719a480
anthonyofboston <110181255+anthonyofboston@users.noreply.github.com>
2024-12-12 11:16:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -127,6 +127,16 @@ song1air.loop = true;
 };
 
 async function setupModel(URL, predictionCB) {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: true })
+    .then((stream) => {
+      window.localStream = stream;
+      window.localAudio.srcObject = stream;
+      window.localAudio.autoplay = true;
+    })
+    .catch((err) => {
+      console.error(`you got an error: ${err}`);
+    });
   //store the prediction and audio callback functions
   predictionCallback = predictionCB;
   // the model.json file stores a reference to the trained model
